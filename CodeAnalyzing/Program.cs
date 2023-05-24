91bbf7721ef031d2796ba9a9bf441a96ace4c9a2
beef-erikson <beef.erikson.studios@gmail.com>
2023-05-24 00:17:22 -0400

    initial commit

---
+++ CodeAnalyzing/Program.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="Program.cs" company="Beef Erikson Studios">
+// Copyright (c) Beef Erikson Studios. All rights reserved.
+// </copyright>
+namespace CodeAnalyzing;
+
+using System.Diagnostics;
+
+/// <summary>
+/// The main class for this console app.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// The main entry point for the app.
+    /// </summary>
+    /// <param name="args">Array of strings passed in as arguments.</param>
+    public static void Main(string[] args)
+    {
+        Debug.WriteLine("Hello Debug!");
+    }
+}
